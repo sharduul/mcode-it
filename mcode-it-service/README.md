b2b2122d7d3ed24282bb56ca37a7d6bf9289edcc
Shardul Bagade <bagadeshardul@gmail.com>
2014-09-30 23:09:11 -0400

    changes for testing heroku app

--- mcode-it-service/README.md
+++ mcode-it-service/README.md
@@ -3,3 +3,9 @@ mcode-it-service
 
 check the app here-
 http://mcode-it-service.herokuapp.com/now
+
+
+to run:
+1. cd into the repo -> mcode-it
+2. git remote set-url heroku git@heroku.com:mcode-it-service.git
+3. git subtree push --prefix mcode-it-service heroku master
--- mcode-it-service/app/routes.js
+++ mcode-it-service/app/routes.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
 	
 	app.get('/now', function(request, response) {
 	  var d = new Date();
-	  response.send(200, {date: d}); // (status code, response body)
+	  response.send(200, {date: "test 1"}); // (status code, response body)
 	});
 
 };

76eea437832d759f4f0114900abb3bdd97444478
Shardul Bagade <bagadeshardul@gmail.com>
2014-10-05 00:07:05 -0400

    service route modified for testing again!

--- mcode-it-service/app/routes.js
+++ mcode-it-service/app/routes.js
@@ -5,7 +5,11 @@ module.exports = function(app) {
 	
 	app.get('/now', function(request, response) {
 	  var d = new Date();
-	  response.send(200, {date: "test 1"}); // (status code, response body)
+	  response.send(200, 
+	  	{date: "test 1",
+	  		port: app.get('port')}); // (status code, response body)
+	  
+
 	});
 
 };

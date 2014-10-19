d07962de524bf1a37b23a8005167516694140331
Shardul Bagade <bagadeshardul@gmail.com>
2014-10-19 16:16:51 -0400

    Revert "sharejs setup complete on local machine"
    This reverts commit d4fc05a41ba19868dec5bd5eacf26bc31e4afd1d.

--- mcode-it-service/server.js
+++ mcode-it-service/server.js
@@ -1,20 +1,12 @@
 
 var express = require('express'),
-http = require('http'),
-sharejs = require('share').server;
+http = require('http');
 var app = express();
 var server = http.createServer(app); 
-
 
 //app.use(express.static(__dirname + '/public'));
 app.set('port', process.env.PORT || 8000); // set the port number as environment variable
 //app.set('port', 3000); // set the port number as environment variable
-
-
-var options = {db:{type:'none'}};
-sharejs.attach(app, options);
-
-app.use(express.static(__dirname + '/public'));
 
 // routes ======================================================================
 require('./app/routes.js')(app);

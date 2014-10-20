e159a339d840df1120f3f7d2b47d4e2696bc8086
Shardul Bagade <bagadeshardul@gmail.com>
2014-10-19 21:05:26 -0400

    app: templates and routing setup completed

---
+++ mcode-it-app/README.md
@@ -0,0 +1,6 @@
+mcode-it-app
+================
+
+Refer these examples for development:
+- https://github.com/driftyco/ionic-contrib-firebase-login/blob/master/www%2Fjs%2Fapp.js
+- https://github.com/driftyco/ionic-starter-sidemenu/blob/master/templates%2Fmenu.html
--- mcode-it-app/www/js/app.js
+++ mcode-it-app/www/js/app.js
@@ -1,12 +1,9 @@
-// Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('mcodeit', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
+
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if(window.cordova && window.cordova.plugins.Keyboard) {
@@ -16,4 +13,39 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
+})
+
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  
+  $stateProvider
+
+    .state('splash', {
+      url: "/",
+      templateUrl: "templates/splash.html"
+    })
+
+    .state('code', {
+      url: "/code",
+      templateUrl: "templates/code.html",
+      controller: 'CodeCtrl'
+    })
+
+    .state('result', {
+      url: '/result',
+      templateUrl: 'templates/result.html',
+      controller: 'ResultCtrl'
+    })
+
+    // show code and result pane on single view
+    .state('combined', {
+      url: '/combined',
+      templateUrl: 'templates/combined.html',
+      controller: 'CombinedCtrl'
+    })
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/');
+
 })

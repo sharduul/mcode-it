08294196b1a3e233cb2698d2c624a83bdc1a67f8
Shardul Bagade <bagadeshardul@gmail.com>
2014-11-01 02:26:48 -0400

    http request made to the server. program is now running remotely.

--- mcode-it-app/www/js/services/Code.js
+++ mcode-it-app/www/js/services/Code.js
@@ -2,9 +2,9 @@ angular
 	.module('mcodeit.services')
 	.factory('Code', Code);
 
-Code.$inject = [];
+Code.$inject = ['$http'];
 
-function Code() {
+function Code($http) {
   
 	var service = {
 		Run : Run
@@ -15,7 +15,19 @@ function Code() {
 
 	function Run(codeText){
 
-		console.log(codeText);
+		var responsePromise =  $http({
+										url: "http://localhost:8000/api/run", 
+										//url: "http://192.168.0.101:8000/api/run", 
+										method: "GET",
+										params: {codeText : codeText}
+									 });
+
+		responsePromise.success(function(data, status, headers, config) {
+			console.log(data);
+		});
+		responsePromise.error(function(data, status, headers, config) {
+			console.log("error");
+		});
 
 	}
 
